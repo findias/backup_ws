cc3dd9ac20959201181f14fe0033ae0d97ee3e7c
Konstantin Kovalev <findiask@gmail.com>
2022-10-31 22:59:23 +0300

    add function 'find_last_file'. This function search last backup archive in backup folder

--- backup.py
+++ backup.py
@@ -14,7 +14,8 @@ user_name = 'konkov'
 home_dir = "/home/" + user_name
 backup_folder = home_dir + '/Documents/scripts/backup_folder'
 arch_name = 'backup_' + time.strftime("%d_%m_%Y_%H-%M")
-arch_path = '/opt/backup/' + arch_name
+arch_path = '/opt/backup/'
+arch_file_path = arch_path + arch_name
 configs_file = home_dir + '/.config'
 configs_file_for_backup = {
     'ssh':      [home_dir + '/.ssh/', []],
@@ -48,15 +49,28 @@ def create_backup(backup_files, dst_path):
                 shutil.copy(src_copy_path, gen_path)
                 logging.info("Config " + key + " is copy in " + gen_path + file)
 
+''' Get last archive'''
+def find_last_file(dir):
+    logging.info("----==== Start find and get last backup archive ====----")
+    f_list = os.listdir(dir)
+    if f_list:
+        f_list = [os.path.join(dir, file) for file in f_list]
+        f_list = [file for file in f_list if os.path.isfile(file)]
+        last_file = max(f_list, key=os.path.getctime)
+        logging.info("----====" + last_file + " is last achive ====----")
+        return last_file
+    else:
+        logging.warning("----==== Current folder " + f_list + " is empty ====----")
 
 def archive_backup():
     logging.info('----==== Create archive ====-----')
-    shutil.make_archive(arch_path, 'gztar', backup_folder)
+    shutil.make_archive(arch_file_path, 'gztar', backup_folder)
 
 
 if __name__ == '__main__':
     if (os.name == 'posix') and (user_name == os.getlogin()):  # Check OS and username
         try:
+            find_last_file(arch_path)
             create_backup(configs_file_for_backup, backup_folder)
             archive_backup()
         except Exception as e:
