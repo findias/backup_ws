9489eb6b05e941d1f37f88014d3f7d8a50cda8d1
Konstantin Kovalev <findiask@gmail.com>
2023-02-12 13:50:17 +0300

    add curr_backup

--- backup.py
+++ backup.py
@@ -51,7 +51,7 @@ configs_file_for_backup = {
     'picom':    [os.path.join(configs_file, 'picom/'), ['picom.conf']],
     'rofi':     [os.path.join(configs_file, 'rofi/'), ['config.rasi']],
     'root':     [home_dir, ['.zshrc', '.zshrc.pre-oh-my-zsh', '.zsh_history', 'first_sc.sh']],
-    'my_data':  [os.path.join(doc_dir, 'bd_pass/'), []],
+    # 'my_data':  [os.path.join(doc_dir, 'bd_pass/'), []],
     'stocks':   [os.path.join(doc_dir,  'stocks/'), []],
     'db_pg':    [os.path.join(doc_dir, 'bd_postgress/'), []],
     'alacritty':[os.path.join(configs_file, 'alacritty/'), []],
